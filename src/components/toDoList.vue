d7ff4d270236c7d0f6660dcdf376b98766ba2380
silvia76-max <silviarodriguezexposito.76@gmail.com>
2025-02-16 21:40:05 +0100

    agregadas nuevas funcionalidades

--- src/components/toDoList.vue
+++ src/components/toDoList.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <h2>Lista de Tareas</h2>
+    
     <input v-model="nuevaTarea" @keyup.enter="agregarTarea" placeholder="Añadir tarea" />
     <button @click="agregarTarea">Agregar</button>
-    <ul>
-      <li v-for="(tarea, index) in tareas" :key="index">
-        {{ tarea }}
-        <button @click="eliminarTarea(index)">❌</button>
-      </li>
-    </ul>
+    
+      <ul>
+  <li v-for="(tarea, index) in tareas" :key="index" :class="{ completada: tarea.completada }">
+    <span v-if="!tarea.editando" @click="toggleCompletada(index)">{{ tarea.texto }}</span>
+    <input v-else v-model="tarea.texto" @keyup.enter="guardarEdicion(index)" @blur="guardarEdicion(index)" />
+    
+    <button @click="editarTarea(index)">✏️</button>
+    <button @click="eliminarTarea(index)">❌</button>
+  </li>
+</ul>
+
   </div>
 </template>
 
@@ -23,13 +29,45 @@ export default {
   methods: {
     agregarTarea() {
       if (this.nuevaTarea.trim() !== "") {
-        this.tareas.push(this.nuevaTarea);
+        this.tareas.push({ texto: this.nuevaTarea, completada: false });
         this.nuevaTarea = "";
+        this.guardarEnLocalStorage();
       }
+        },
+        editarTarea(index) {
+      this.tareas[index].editando = true;
     },
+      guardarEdicion(index) {
+      this.tareas[index].editando = false;
+      this.guardarEnLocalStorage(); // Guardamos el cambio en localStorage
+      }
+      agregarTarea() {
+    if (this.nuevaTarea.trim() !== "") {
+      this.tareas.push({ texto: this.nuevaTarea, completada: false, editando: false });
+      this.nuevaTarea = "";
+      this.guardarEnLocalStorage();
+    }
+  }
     eliminarTarea(index) {
       this.tareas.splice(index, 1);
+      this.guardarEnLocalStorage();
+    },
+    toggleCompletada(index) {
+      this.tareas[index].completada = !this.tareas[index].completada;
+      this.guardarEnLocalStorage();
+    },
+    guardarEnLocalStorage() {
+      localStorage.setItem("tareas", JSON.stringify(this.tareas));
+    },
+    cargarDesdeLocalStorage() {
+      const tareasGuardadas = localStorage.getItem("tareas");
+      if (tareasGuardadas) {
+        this.tareas = JSON.parse(tareasGuardadas);
+      }
     }
+  },
+  created() {
+    this.cargarDesdeLocalStorage();
   }
 };
 </script>
@@ -39,7 +77,28 @@ input {
   padding: 5px;
   margin-right: 10px;
 }
+
 button {
   cursor: pointer;
+  margin-left: 5px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 5px 0;
+  padding: 5px;
+  border-bottom: 1px solid #ccc;
+}
+
+.completada {
+  text-decoration: line-through;
+  color: gray;
 }
 </style>
